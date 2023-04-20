279ddcbd1f2606c903758f57a2bb3e30defbe433
langyamu <1764714737@qq.com>
2023-04-20 23:40:34 +0800

    :alembic:Schema

--- src/App.vue
+++ src/App.vue
@@ -12,36 +12,57 @@ let editorRef = ref<HTMLElement>();
 
 onMounted(() => {
 
-let modelSchema = new Schema({
-    nodes: addListNodes(schema.spec.nodes as any,"paragraph block*", "block"),
-    marks:schema.spec.marks,
-})
-
-  let state = EditorState.create({ 
+  // 创建一个新的  ProseMirror 文档规范对象 docSchema
+  // Schema: 对编辑器 dom 结构、规则的抽象描述的对象，类似 编辑器DOM 转换为 JS Object 之前先定义 JS Object 结构的过程
+  // 是一个抽象描述 编辑器实例 DOM 结构和规则的对象，可以理解为编辑器 dom 的 js 对象模板，它规定了 编辑器 dom 内可出现的  node 类型、属性、样式，以及 node 之间的关系
+  let docSchema = new Schema({
+    nodes: addListNodes(schema.spec.nodes as any, "paragraph block*", "block"),
+    marks: schema.spec.marks,
+  })
+  // 默认支持的节点类型
+  //  文本节点
+  //  块级节点
+  // 内联节点
+    console.log("🚀 ~ file: App.vue:22 ~ onMounted ~ schema:", schema)
+  //  docSchema 
+  //  markFromJSON
+  // nodeFromJSON
+  console.log("🚀 ~ file: App.vue:22 ~ onMounted ~ docSchema:", docSchema)
+  // @ts-ignore
+  let state = EditorState.create({
     schema,
-    doc:DOMParser.fromSchema(modelSchema).parse(editorRef.value as HTMLElement),
-    plugins:exampleSetup({schema:modelSchema})
-});
-  let view = new EditorView(editorRef.value as HTMLElement,{
+    doc: DOMParser.fromSchema(docSchema).parse(editorRef.value as HTMLElement),
+    plugins: exampleSetup({ schema: docSchema, menuBar: false })
+  });
+  let view = new EditorView(editorRef.value as HTMLElement, {
     state,
   });
+
+  editorRef.value?.focus()
 });
 </script>   
 
 <template>
-  <div id="editor" ref="editorRef" contenteditable ></div>
+  <div id="editor" ref="editorRef" contenteditable></div>
 </template>
 <style lang="css">
-    @import url("./css/reset.css");
-    @import url("/node_modules/prosemirror-view/style/prosemirror.css");
-    @import url("/node_modules/prosemirror-menu/style/menu.css");
-    @import url("/node_modules/prosemirror-gapcursor/style/gapcursor.css");
-    @import url("/node_modules/prosemirror-example-setup/style/style.css");
-</style>
-<style scoped>
+@import url("./css/reset.css");
+@import url("/node_modules/prosemirror-view/style/prosemirror.css");
+@import url("/node_modules/prosemirror-menu/style/menu.css");
+@import url("/node_modules/prosemirror-gapcursor/style/gapcursor.css");
+@import url("/node_modules/prosemirror-example-setup/style/style.css");
 
-#editor {
-  width: 50vw;
-  height: 50vh;
+.ProseMirror {
+  width: 210mm;
+  height: 297mm;
 }
 </style>
+<style scoped>
+#editor {
+  width: 220mm;
+  height: 50vh;
+  overflow-y: scroll;
+  padding: 1em 2em;
+}
+</style>
+
